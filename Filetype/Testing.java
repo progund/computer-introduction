3f526415274115e6d1cbcec303937b82c91218d7
Henrik Sandklef <hesa@sandklef.com>
2016-06-20 13:35:16 +0200

    First rev

---
+++ Filetype/Testing.java
@@ -0,0 +1,9 @@
+// Java class are normally (always) in so called packages, but in this
+// exercise we skip that
+
+public class Testing { 
+
+    // This is really not a very useful class, but good enough to use
+    // for checking file types.
+
+}
